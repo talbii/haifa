33471c6eb69833f2a1fd7d07e51e8fbb7bf0bf2e
Alon (Ubuntu) <downtown2u@gmail.com>
2022-05-15 03:26:34 +0300

    ✨ Added `rubiks-tree` sample solution

---
+++ 05-basic-dp/problems/B/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int MOD = 1e9 + 7;
+inline int mod(int a) { return a %= MOD, a + (a < 0 ? a : 0); };
+signed main() {
+    int n;
+    cin >> n;
+    int ans = 1;
+    for (int k = 2; k <= n; k++) ans = mod(mod(ans * ans) * 16);
+    cout << mod(ans * 6) << endl;
+}
